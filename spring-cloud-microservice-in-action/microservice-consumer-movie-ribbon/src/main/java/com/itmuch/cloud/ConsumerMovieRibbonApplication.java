4ba31ba968be1e26d5673cbe700833f0f0b5a42e
sunqi <sunqi@viewhigh.com>
2018-07-18 15:36:44 +0800

    代码提交

--- spring-cloud-microservice-in-action/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
+++ spring-cloud-microservice-in-action/microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/ConsumerMovieRibbonApplication.java
@@ -5,14 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
 @SpringBootApplication
 @EnableEurekaClient
-@RibbonClient(name = "microservice-provider-user", configuration = TestConfiguration.class)
+//指定单个服务的配置，如果想让所有的ribbon策略都一样 则@ComponentScan.Filter去掉
+@RibbonClients(value = {
+        @RibbonClient(name = "microservice-provider-user", configuration = TestConfiguration.class),
+        @RibbonClient(name = "microservice-provider-user2", configuration = TestConfiguration.class)})
+
 @ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class) })
 public class ConsumerMovieRibbonApplication {
 
@@ -25,4 +34,5 @@ public class ConsumerMovieRibbonApplication {
   public static void main(String[] args) {
     SpringApplication.run(ConsumerMovieRibbonApplication.class, args);
   }
+ 
 }
